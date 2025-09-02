ce043506e8d3fb66990a010113755fcd1e58a5ca
Kartikey <kartikey442004@gmail.com>
2025-09-02 22:38:01 +0530

    Initial commit - backend

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import { connectDB } from './lib/db.js';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+const app = express();
+const PORT = process.env.PORT || 5001;
+app.use(cors(
+  {
+    origin: "http://localhost:5001",
+    credentials: true,
+  }
+));
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB()
+  .then(() => console.log('Database connected successfully'))
+  .catch((error) => console.error('Database connection failed:', error));
+
+});
+
